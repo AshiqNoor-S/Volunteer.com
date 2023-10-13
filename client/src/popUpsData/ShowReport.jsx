f9ff170ca876a93dab1a644f3658c7361089ae1d
VISHAL KUAMR YADAV <vishal100403@gmail.com>
2023-10-13 11:20:14 +0530

    Report Issue Part Done

--- client/src/popUpsData/ShowReport.jsx
+++ client/src/popUpsData/ShowReport.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { PopUpStyle } from "../styles/PopUpStyle";
+import styled from "styled-components";
 
 const ShowReport = ({ closeModal }) => {
   return (
@@ -7,18 +8,67 @@ const ShowReport = ({ closeModal }) => {
       <div className="modal-wrapper" onClick={closeModal}></div>
       <div className="modal-container">
         <h2>Report An Issue</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-          nisi dignissimos mollitia ex ut tenetur commodi facilis tempore,
-          quidem ipsa magnam autem ad numquam temporibus harum enim recusandae.
-          Id, impedit. Ab, praesentium? Veritatis eaque temporibus aperiam
-          obcaecati corporis soluta iste quo. Itaque est, quasi minima
-          voluptatem voluptatum magnam omnis repellat.
-        </p>
+        <Wrapper>
+          <div className="contact-form">
+            <form
+              method="POST"
+              action="https://formspree.io/f/xgejpzza"
+              className="contact-inputs"
+            >
+              <input
+                type="text"
+                placeholder="User name"
+                name="username"
+                required
+                autoComplete="off"
+              />
+              <input
+                type="email"
+                name="email"
+                required
+                placeholder="Email"
+                autoComplete="off"
+              />
+              <textarea
+                placeholder="Enter your message"
+                autoComplete="off"
+                cols="30"
+                rows="10"
+                required
+              />
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+        </Wrapper>
         <button onClick={closeModal}>Close</button>
       </div>
     </PopUpStyle>
   );
 };
+
+const Wrapper = styled.section`
+  .contact-form {
+    max-width: 50rem;
+    margin: auto;
+
+    .contact-inputs {
+      display: flex;
+      flex-direction: column;
+      gap: 3rem;
+
+      input[type="submit"] {
+        cursor: pointer;
+        transition: all 0.2s;
+
+        &:hover {
+          background-color: ${({ theme }) => theme.colors.white};
+          border: 1px solid ${({ theme }) => theme.colors.btn};
+          color: ${({ theme }) => theme.colors.btn};
+          transform: scale(0.9);
+        }
+      }
+    }
+  }
+`;
 
 export default ShowReport;
