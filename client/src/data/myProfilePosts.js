f3c17f8407a667b785b01adaa82c015b6b81e99f
Priyanshu Pattanaik <hi_priyanshu@outlook.com>
2023-10-13 20:28:51 +0530

    Profile page done

---
+++ client/src/data/myProfilePosts.js
@@ -0,0 +1,34 @@
+const myProfilePosts = [
+	{
+		id: 1,
+		postURL: '/',
+		imgURL: './images/spinner.gif'
+	},
+	{
+		id: 2,
+		postURL: '/',
+		imgURL: './images/spinner.gif'
+	},
+	{
+		id: 3,
+		postURL: '/',
+		imgURL: './images/spinner.gif'
+	},
+	{
+		id: 4,
+		postURL: '/',
+		imgURL: './images/spinner.gif'
+	},
+	{
+		id: 5,
+		postURL: '/',
+		imgURL: './images/spinner.gif'
+	},
+	{
+		id: 6,
+		postURL: '/',
+		imgURL: './images/spinner.gif'
+	},
+]
+
+export default myProfilePosts;
--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import myProfilePosts from "../data/myProfilePosts";
+import { ProfileStyle } from "../styles/ProfileStyle";
 
 const MyProfile = () => {
-  return <div>My Profile work should be done over here</div>;
+  return (
+    <ProfileStyle>
+      <div className="profile-banner-imgs" >
+        <img src="./images/profile_banner.jpg" className="banner" style={{ width: '100%' }} />
+        <img src="./images/user_icon.png" className="profile-pic" />
+      </div>
+      <div className="heading-text">
+        <h1 className="user-name">My Profile</h1>
+        <span className="profession-tag">profession</span>
+      </div>
+
+      <div className="about-me">
+        <h1 className="about-me-heading">About Me</h1>
+        <p className="about-me-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis risus sed elit bibendum ultrices. Duis vel sapien varius, sagittis risus sit amet, placerat nulla Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis risus sed elit bibendum ultrices. Duis vel sapien varius, sagittis risus sit amet, placerat nulla Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis risus sed elit bibendum ultrices. Duis vel sapien varius, sagittis risus sit amet, placerat nulla</p>
+      </div>
+
+      <div className="my-posts">
+        <h1 className="my-posts-heading">My Posts</h1>
+        {myProfilePosts.map((post) => (
+          <a href={post.postURL} ><img src={post.imgURL} className="postBanner" /></a>
+        ))}
+      </div>
+
+    </ProfileStyle>
+  );
 };
 
 export default MyProfile;
