296d8e1b851dde73003715b9790a9d19c00df67f
VISHAL KUAMR YADAV <vishal100403@gmail.com>
2023-10-16 11:58:07 +0530

    Modified profile page

--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -5,9 +5,13 @@ import { ProfileStyle } from "../styles/ProfileStyle";
 const MyProfile = () => {
   return (
     <ProfileStyle>
-      <div className="profile-banner-imgs" >
-        <img src="./images/profile_banner.jpg" className="banner" style={{ width: '100%' }} />
-        <img src="./images/user_icon.png" className="profile-pic" />
+      <div className="profile-banner-imgs">
+        <img
+          src="./images/profile_banner4.4.png"
+          className="banner"
+          style={{ width: "100%" }}
+        />
+        <img src="./images/user_icon3.png" className="profile-pic" />
       </div>
       <div className="heading-text">
         <h1 className="user-name">My Profile</h1>
@@ -16,16 +20,26 @@ const MyProfile = () => {
 
       <div className="about-me">
         <h1 className="about-me-heading">About Me</h1>
-        <p className="about-me-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis risus sed elit bibendum ultrices. Duis vel sapien varius, sagittis risus sit amet, placerat nulla Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis risus sed elit bibendum ultrices. Duis vel sapien varius, sagittis risus sit amet, placerat nulla Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis risus sed elit bibendum ultrices. Duis vel sapien varius, sagittis risus sit amet, placerat nulla</p>
+        <p className="about-me-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mattis
+          risus sed elit bibendum ultrices. Duis vel sapien varius, sagittis
+          risus sit amet, placerat nulla Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Fusce mattis risus sed elit bibendum ultrices. Duis
+          vel sapien varius, sagittis risus sit amet, placerat nulla Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit. Fusce mattis risus sed
+          elit bibendum ultrices. Duis vel sapien varius, sagittis risus sit
+          amet, placerat nulla
+        </p>
       </div>
 
       <div className="my-posts">
         <h1 className="my-posts-heading">My Posts</h1>
         {myProfilePosts.map((post) => (
-          <a href={post.postURL} ><img src={post.imgURL} className="postBanner" /></a>
+          <a href={post.postURL}>
+            <img src={post.imgURL} className="postBanner" />
+          </a>
         ))}
       </div>
-
     </ProfileStyle>
   );
 };
